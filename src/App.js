59e51890c2ece844e3cbddc5c6054d991f4cebab
saleembaig <gojalisaleem1@gmail.com>
2021-01-24 21:00:34 +0500

    Update App.js
    added Suspense for data fetchting info

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import React, { Suspense } from 'react';
 import './App.css';
+import MainApp from './components/MainApp';
+import image from "./image.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <img src={image} alt="covid-19" height="20%" width="20%" />
+      <Suspense fallback={<h4>Loading data from API...</h4>}>
+        <MainApp />
+      </Suspense>
     </div>
   );
 }
